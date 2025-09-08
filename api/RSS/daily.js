5cc2fde57a96580f8b7e14c362e944fb306381f9
jeong-hk0316 <dnguicdi@gmail.com>
2025-09-08 15:09:53 +0900

    Update daily.js

--- api/RSS/daily.js
+++ api/RSS/daily.js
@@ -1,1 +1,6 @@
+const { FEEDS } = require("./lib/rssFeeds");
+const { fetchRSS, fetchHTMLList, normalizeRSSItem } = require("./lib/fetchers");
+const { dedupe } = require("./lib/dedupe");
+const { summarizeKo20 } = require("./lib/summarizer");
+const { dateRangeKST, formatYMDKST } = require("./lib/timeKST");
 
