4d30c54e4a3d6c018e1587e9ea54841677a6dacd
jeong-hk0316 <dnguicdi@gmail.com>
2025-09-08 15:53:11 +0900

    Update daily.js

--- api/RSS/daily.js
+++ api/RSS/daily.js
@@ -1,5 +1,20 @@
-const { FEEDS } = require("./lib/rssfeeds");
-const { fetchRSS, fetchHTMLList, normalizeRSSItem } = require("./lib/fetchers");
-const { dedupe } = require("./lib/dedupe");
-const { summarizeKo20 } = require("./lib/summarizer");
-const { dateRangeKST, formatYMDKST } = require("./lib/timeKST");
+// api/rss/daily.js (임시 테스트)
+try {
+  const { FEEDS } = require("./lib/rssFeeds");
+  const { dateRangeKST } = require("./lib/timeKST");
+  
+  module.exports = (req, res) => {
+    res.status(200).json({ 
+      message: "파일 로드 성공!",
+      feedsCount: FEEDS ? FEEDS.length : 0,
+      dateTest: dateRangeKST()
+    });
+  };
+} catch (error) {
+  module.exports = (req, res) => {
+    res.status(500).json({ 
+      error: "파일 로드 실패",
+      message: error.message 
+    });
+  };
+}
