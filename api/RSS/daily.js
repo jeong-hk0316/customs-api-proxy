804931b1618aa5306b20c853d78d0d7b86a40225
jeong-hk0316 <dnguicdi@gmail.com>
2025-09-08 15:59:56 +0900

    Update daily.js

--- api/RSS/daily.js
+++ api/RSS/daily.js
@@ -1,7 +1,3 @@
 module.exports = (req, res) => {
-  res.status(200).json({
-    message: "API 작동 테스트",
-    timestamp: new Date().toISOString(),
-    path: req.url
-  });
+  res.status(200).send("Hello World!");
 };
